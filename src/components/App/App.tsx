7e765c6f13f48a3f160977492fb27882ae5e7856
Andrey <antonov.andrey.10101@gmail.com>
2023-03-01 10:13:45 +0300

    feat: add favicon

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Box} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Thanks from '../Thanks/Thanks';
 import Rating from '../Rating/Rating';
 
 function App() {
   const [isSubmit, setIsSubmit] = useState(false);
 
-  return <Box>{isSubmit ? <Thanks /> : <Rating submit={setIsSubmit}/>}</Box>;
+  return <Box>{isSubmit ? <Thanks /> : <Rating submit={setIsSubmit} />}</Box>;
 }
 
 export default App;
